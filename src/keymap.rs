eb18ce4fc7d65b2e7cd1a4b0d8ad133603a10e70
hinshiba <105423175+d1b26@users.noreply.github.com>
2025-09-06 15:48:28 +0900

    feat: 2x4に変更

--- src/keymap.rs
+++ src/keymap.rs
@@ -1,6 +1,6 @@
 use rmk::action::KeyAction;
 use rmk::{k, layer, mo};
-pub(crate) const COL: usize = 3;
+pub(crate) const COL: usize = 4;
 pub(crate) const ROW: usize = 2;
 pub(crate) const NUM_LAYER: usize = 2;
 
@@ -8,12 +8,12 @@ pub(crate) const NUM_LAYER: usize = 2;
 pub const fn get_default_keymap() -> [[[KeyAction; COL]; ROW]; NUM_LAYER] {
     [
         layer!([
-            [k!(A), k!(B), k!(Backspace)],
-            [k!(C), k!(D), mo!(1)]
+            [k!(A), k!(B), k!(Backspace), k!(G)],
+            [k!(C), k!(D), mo!(1), k!(H)]
         ]),
         layer!([
-            [k!(E), k!(F), k!(Enter)],
-            [k!(Kc4), k!(Kc5), k!(Kc6)]
+            [k!(E), k!(F), k!(Enter), k!(I)],
+            [k!(Kc4), k!(Kc5), k!(Kc6), k!(J)]
         ]),
     ]
 }
--- src/main.rs
+++ src/main.rs
@@ -85,7 +85,7 @@ async fn main(_s: Spawner) {
     let flash = async_flash_wrapper(flash);
 
     // Initialize the IO pins
-    let (input_pins, output_pins) = config_matrix_pins_esp!(peripherals: peripherals, input: [GPIO20, GPIO21], output: [GPIO2, GPIO22, GPIO23]);
+    let (input_pins, output_pins) = config_matrix_pins_esp!(peripherals: peripherals, input: [GPIO20, GPIO21], output: [GPIO2, GPIO22, GPIO23, GPIO1]);
 
     // RMK config
     let vial_config = VialConfig::new(VIAL_KEYBOARD_ID, VIAL_KEYBOARD_DEF);
